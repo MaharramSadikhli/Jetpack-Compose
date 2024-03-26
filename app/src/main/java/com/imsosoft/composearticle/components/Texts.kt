e5f0d1dda9db30ac2a1d108db10302a5a128b514
Muarem <s.muarem10@gmail.com>
2024-03-26 21:25:10 +0400

    created text compose components

---
+++ app/src/main/java/com/imsosoft/composearticle/components/Texts.kt
@@ -0,0 +1,43 @@
+package com.imsosoft.composearticle.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TitleText( text: String ) {
+
+    Text(
+        text = text,
+        fontSize = 24.sp,
+        modifier =  Modifier.padding(16.dp)
+    )
+
+}
+
+@Composable
+fun IntroText( text: String ) {
+
+    Text(
+        text = text,
+        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+        textAlign = TextAlign.Justify
+        )
+
+}
+
+
+@Composable
+fun DescriptionText( text: String ) {
+
+    Text(
+        text = text,
+        modifier = Modifier.padding(16.dp),
+        textAlign = TextAlign.Justify
+        )
+
+}
