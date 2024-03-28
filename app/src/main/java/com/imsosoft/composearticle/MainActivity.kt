885382d23465b50af9026caf518bab4ef6d79cac
Muarem <s.muarem10@gmail.com>
2024-03-28 19:55:28 +0400

    task 2 completed

--- app/src/main/java/com/imsosoft/composearticle/MainActivity.kt
+++ app/src/main/java/com/imsosoft/composearticle/MainActivity.kt
@@ -4,24 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.imsosoft.composearticle.composearticle.ComposeArticle
-import com.imsosoft.composearticle.composearticle.components.DescriptionText
-import com.imsosoft.composearticle.composearticle.components.IntroText
-import com.imsosoft.composearticle.composearticle.components.TitleText
 import com.imsosoft.composearticle.taskmanager.TaskManager
 import com.imsosoft.composearticle.ui.theme.ComposeArticleTheme
 
@@ -33,8 +19,7 @@ class MainActivity : ComponentActivity() {
             ComposeArticleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     
-                    TaskManager(modifier = Modifier.padding(innerPadding))
-                    
+
                 }
             }
         }
