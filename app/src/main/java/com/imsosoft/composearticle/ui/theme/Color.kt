0f11cd1c220a41c75a266f792e0ff9839a6cd092
Muarem <s.muarem10@gmail.com>
2024-03-28 20:05:43 +0400

    added colors for task 3

--- app/src/main/java/com/imsosoft/composearticle/ui/theme/Color.kt
+++ app/src/main/java/com/imsosoft/composearticle/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val QuadrantWhite = Color(0xFFEADDFF)
+val QuadrantLightPurple = Color(0xFFD0BCFF)
+val QuadrantPurple = Color(0xFFB69DF8)
+val QuadrantExtraLight = Color(0xFFF6EDFF)
