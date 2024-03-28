0001f9b425c5a3b31ab5cd6a53bb083c4aee4043
Muarem <s.muarem10@gmail.com>
2024-03-28 20:57:56 +0400

    task 3 completed

--- app/src/main/java/com/imsosoft/composearticle/MainActivity.kt
+++ app/src/main/java/com/imsosoft/composearticle/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.imsosoft.composearticle.composequadrant.ComposeQuadrant
-import com.imsosoft.composearticle.taskmanager.TaskManager
 import com.imsosoft.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeArticleTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    ComposeQuadrant()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
                 }
             }
         }
